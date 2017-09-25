9d0c766f5a1862e694dc19ff8a791139b0363da1
dz <david@nodnod.net>
2017-09-25 17:53:06 -0400

    undefascore thea mthoesd

--- swirl.rb
+++ swirl.rb
@@ -14,26 +14,14 @@ class Swirl
     @db = db
   end
 
-  def db_has_word?(word)
-    @db && @db.respond_to?(:has_word?) ? @db.has_word?(word) : false
-  end
-
-  def db_word_link(word)
-    if @db && @db.respond_to?(:get_links_for_word)
-      @db.get_links_for_word(word).sort_by { |i| i[:amount] }.reverse.first[:url]
-    else
-      ""
-    end
-  end
-
   def money_making_html
     frag = Nokogiri::HTML.fragment(@html)
     frag.traverse do |n|
       if n.text?
         n.replace(n.content.split(" ").map do |word|
           clean_word = word.strip.downcase.gsub(/\W+/, '')
-          if db_has_word?(clean_word) && !db_word_link(clean_word).empty?
-            "<a href=\"#{db_word_link(clean_word)}\">#{word}</a>"
+          if _db_has_word?(clean_word) && !_db_word_link(clean_word).empty?
+            "<a href=\"#{_db_word_link(clean_word)}\">#{word}</a>"
           else
             word
           end
@@ -42,4 +30,17 @@ class Swirl
     end
     frag.to_html
   end
+
+  def _db_has_word?(word)
+    @db && @db.respond_to?(:has_word?) ? @db.has_word?(word) : false
+  end
+
+  def _db_word_link(word)
+    if @db && @db.respond_to?(:get_links_for_word)
+      @db.get_links_for_word(word).sort_by { |i| i[:amount] }.reverse.first[:url]
+    else
+      ""
+    end
+  end
+
 end
